bd4f35066c949441c25236417c70881095791fdb
LordHelpUs <mgelban@gmail.com>
2018-03-09 17:31:52 -0800

    no sandbox

--- helpers/citytrash.js
+++ helpers/citytrash.js
@@ -6,7 +6,7 @@ exports.getTrash = () => {
 
 
 exports.getDate = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://apps.sandiego.gov/econtainer/control/searchaddressinfo', {
     waitUntil: 'load'
