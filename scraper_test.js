2b7b8cff7ff5fff3ff40544caea62be8681d68f0
el_gelbo <mgelbman@gmail.com>
2018-09-24 11:54:50 -0700

    update for new city web service

--- scraper_test.js
+++ scraper_test.js
@@ -1,23 +1,22 @@
 const puppeteer = require('puppeteer');
 var moment = require('moment');
 const mongoose = require('mongoose');
-require('../models/Trash');
+require('./models/Trash');
 const Trash = mongoose.model('Trash');
 mongoose.Promise = global.Promise;
-const getDay = require('./getDay');
+const getDay = require('./helpers/getDay');
 
 const url = 'https://getitdone.force.com/ESD_TrashCollectionSchedule';
 
-pupDate = async (name) => {
-// console.log(name);
-  const browser = await puppeteer.launch({ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  // const browser = await puppeteer.launch({headless: false, ignoreHTTPSErrors: true});
-  console.log('i am the pup');
+pupDate = async () => {
+  // console.log(name);
+  // const browser = await puppeteer.launch({ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch({headless: false, ignoreHTTPSErrors: true});
   try {
+    console.log('i am the pup');
     const page = await browser.newPage();
-    await page.goto(url, {
-      waitUntil: 'load'
-    });
+    await page.goto(url);
+
     const streetNumber = '#pgid\\:fmId\\:j_id44\\:j_id45\\:siteStreetNumberId';
     await page.type(streetNumber, '5023');
 
@@ -36,18 +35,14 @@ pupDate = async (name) => {
     await page.click(nxt);
     await page.waitFor(1000);
     const tDate = await page.evaluateHandle(() => document.querySelector('table.twelve > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(4)').textContent);
-    const rDate = await page.evaluateHandle(() => document.querySelector('table.twelve > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(4)').textContent);
-    const trashD = moment(tDate._remoteObject.value.trim(), "MM-DD-YYYY");
-    const recyD = moment(rDate._remoteObject.value.trim(), "MM-DD-YYYY");
-    const newR = moment(recyD).clone().add(7, "hours").toISOString();
-    const newT = moment(trashD).clone().add(7, "hours").toISOString();
-    console.log(newR, newT);
+    console.log(tDate._remoteObject.value.trim());
+    
+
     await browser.close();
-    return getDay.processMoment(name, newT, newR);
   } catch (e) {
     console.log(e);
     await browser.close();
   }
 };
 
-module.exports.pupDate = pupDate;
+pupDate();
