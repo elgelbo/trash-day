18d4ac5f175489fd76a8d4796412e9589227b543
el_gelbo <mgelbman@gmail.com>
2018-11-17 19:53:58 -0800

    fix holiday trigger, setup email

--- controllers/messageControl.js
+++ controllers/messageControl.js
@@ -1,31 +1,59 @@
 const moment = require('moment-timezone');
 const email = require('../handlers/email')
-var checkWindow = exports.checkWindow = (day) => {
-    const timeTill = day.trash.hrsTill;  
-    const now = moment().tz('America/Los_Angeles').format('MMMM Do YYYY, h:mm:ss a z');
+
+altTill = (now) => {
+    if (now.day() <= 5) {
+        const nextT = now.clone().milliseconds(0).seconds(0).minutes(0).hours(8).day(5);
+        return now.diff(nextT, "hours", true);
+    } else {
+        const nextT = now.clone().milliseconds(0).seconds(0).minutes(0).hours(8).day(5).add(1, 'weeks');
+        return now.diff(nextT, "hours", true);
+    }
+}
+
+var check = exports.check = (day) => {
+    const timeTill = day.trash.hrsTill;
+    const now = moment().tz('America/Los_Angeles');
     if (timeTill >= -14.5 && timeTill <= -13.5) {
         return {
             title: 'normPre',
             trigger: true,
-            current: now,
-            tMinus: day.trash.hrsTill,
+            current: now.format('MMMM Do YYYY, h:mm:ss a z'),
+            tMinus: timeTill,
             it: day.trash.date
         };
     } else if (timeTill >= -1.5 && timeTill <= -0.5) {
         return {
             title: 'normDo',
             trigger: true,
-            current: now,
+            current: now.format('MMMM Do YYYY, h:mm:ss a z'),
             tMinus: timeTill,
             it: day.trash.date
         };
     } else if (day.trash.day != 'Friday') {
-        if (
-            timeTill >= -38.5 && timeTill <= -37.5) {
+        console.log('holiday sch');
+        const alty = altTill(now);
+        if (now.day() === 4 && alty >= -14.5 && alty <= -13.5) {
             return {
-                title: 'altPre',
+                title: 'altWarn',
                 trigger: true,
-                current: now,
+                current: now.format('MMMM Do YYYY, h:mm:ss a z'),
+                tMinus: timeTill,
+                it: day.trash.date
+            };
+        } else if (now.day() === 5 && alty >= -1.5 && alty <= -0.5) {
+            return {
+                title: 'altWarnDay',
+                trigger: true,
+                current: now.format('MMMM Do YYYY, h:mm:ss a z'),
+                tMinus: timeTill,
+                it: day.trash.date
+            };
+        } else {
+            return {
+                title: 'none',
+                trigger: false,
+                current: now.format('MMMM Do YYYY, h:mm:ss a z'),
                 tMinus: timeTill,
                 it: day.trash.date
             };
@@ -34,7 +62,7 @@ var checkWindow = exports.checkWindow = (day) => {
         return {
             title: 'none',
             trigger: false,
-            current: now,
+            current: now.format('MMMM Do YYYY, h:mm:ss a z'),
             tMinus: timeTill,
             it: day.trash.date
         };
@@ -42,11 +70,15 @@ var checkWindow = exports.checkWindow = (day) => {
 }
 // TODO: SET UP EMAIL TRANSPORT
 exports.checkWindow = async (req, res) => {
-    const check = await checkWindow(req.body.trashDay);
+    const checked = await check(req.body.trashDay);
+    console.log(checked.title, checked.trigger);
     if (check.title === 'normPre' || 'normDo') {
         await email.sendEmail(req.body.trashDay.message);
-      } else if (title === 'altWarn') {
+    } else if (title === 'altWarn') {
         const message = `No trash pickup on Friday! ${req.body.trashDay.message}`
+        await email.sendEmail(message);
+    } else if (title === 'altWarnDay') {
+        const message = `No trash pickup today! ${req.body.trashDay.message}`
         await email.sendEmail(message);
     }
     res.status(200).end();
